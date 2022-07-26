2c37596968e92562c1d67a93a578dd82dfe89a64
Gabe <contatogabrielvcsantos@gmail.com>
2022-07-26 09:07:28 -0300

    Projeto SoundDeck

---
+++ JavaScript/SoundDeck/assets/JS/scripts.js
@@ -0,0 +1,226 @@
+function ai(){
+    const audio = new Audio('ai.mp3');
+    audio.play();
+    document.getElementById('ai').style.backgroundColor = "#C4FF5C";
+    setTimeout(() => {
+        document.getElementById('ai').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function aiai(){
+    const audio = new Audio('aiai.mp3')
+    audio.play();
+    document.getElementById('aiai').style.backgroundColor = "#E3A42D";
+    setTimeout(() => {
+        document.getElementById('aiai').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function pot(){
+    const audio = new Audio('pot.mp3')
+    audio.play();
+    document.getElementById('pot').style.backgroundColor = "#FA453E";
+    setTimeout(() => {
+        document.getElementById('pot').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function auba(){
+    const audio = new Audio('auba.mp3')
+    audio.play();
+    document.getElementById('auba').style.backgroundColor = "#8E2DE3";    
+    setTimeout(() => {
+        document.getElementById('auba').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function cav(){
+    const audio = new Audio('cav.mp3')
+    audio.play();
+    document.getElementById('cav').style.backgroundColor = "#329BFB"; 
+    setTimeout(() => {
+        document.getElementById('cav').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function chega(){
+    const audio = new Audio('chega.mp3')
+    audio.play();
+    document.getElementById('chega').style.backgroundColor = "#FADD3C"; 
+    setTimeout(() => {
+        document.getElementById('chega').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function danca(){
+    const audio = new Audio('danca.mp3')
+    audio.play();
+    document.getElementById('danca').style.backgroundColor = "#C4FF5C";
+    setTimeout(() => {
+        document.getElementById('danca').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function demais(){
+    const audio = new Audio('demais.mp3')
+    audio.play();
+    document.getElementById('demais').style.backgroundColor = "#E3A42D";
+    setTimeout(() => {
+        document.getElementById('demais').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function brink(){
+    const audio = new Audio('brink.mp3')
+    audio.play();
+    document.getElementById('brink').style.backgroundColor = "#FA453E";
+    setTimeout(() => {
+        document.getElementById('brink').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function gosta(){
+    const audio = new Audio('gosta.mp3')
+    audio.play();
+    document.getElementById('gosta').style.backgroundColor = "#8E2DE3"; 
+    setTimeout(() => {
+        document.getElementById('gosta').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+    
+}
+
+function nao(){
+    const audio = new Audio('nao.mp3')
+    audio.play();
+    document.getElementById('nao').style.backgroundColor = "#F87AFA";
+    setTimeout(() => {
+        document.getElementById('nao').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function pare(){
+    const audio = new Audio('pare.mp3')
+    audio.play();
+    document.getElementById('pare').style.backgroundColor = "#FADD3C"; 
+    setTimeout(() => {
+        document.getElementById('pare').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function qisso(){
+    const audio = new Audio('qisso.mp3')
+    audio.play();
+    document.getElementById('qisso').style.backgroundColor = "#C4FF5C";
+    setTimeout(() => {
+        document.getElementById('qisso').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function papel(){
+    const audio = new Audio('papel.mp3')
+    audio.play();
+    document.getElementById('papel').style.backgroundColor = "#E3A42D";
+    setTimeout(() => {
+        document.getElementById('papel').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function rapaz(){
+    const audio = new Audio('rapaz.mp3')
+    audio.play();
+    document.getElementById('rapaz').style.backgroundColor = "#FA453E";
+    setTimeout(() => {
+        document.getElementById('rapaz').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function atuma(){
+    const audio = new Audio('atuma.mp3')
+    audio.play();
+    document.getElementById('atuma').style.backgroundColor = "#2FE630";
+    setTimeout(() => {
+        document.getElementById('atuma').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function tome(){
+    const audio = new Audio('tome.mp3')
+    audio.play();
+    document.getElementById('tome').style.backgroundColor = "#FADD3C"; 
+    setTimeout(() => {
+        document.getElementById('tome').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function tapa(){
+    const audio = new Audio('tapa.mp3')
+    audio.play();
+    document.getElementById('tapa').style.backgroundColor = "#F87AFA";
+    setTimeout(() => {
+        document.getElementById('tapa').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function uepa(){
+    const audio = new Audio('uepa.mp3')
+    audio.play();
+    document.getElementById('uepa').style.backgroundColor = "#C4FF5C";
+    setTimeout(() => {
+        document.getElementById('uepa').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function ui(){
+    const audio = new Audio('ui.mp3')
+    audio.play();
+    document.getElementById('ui').style.backgroundColor = "#E3A42D";
+    setTimeout(() => {
+        document.getElementById('ui').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function vdanca(){
+    const audio = new Audio('vdanca.mp3')
+    audio.play();
+    document.getElementById('vdanca').style.backgroundColor = "#0FCBFF";
+    setTimeout(() => {
+        document.getElementById('vdanca').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function irra(){
+    const audio = new Audio('irra.mp3')
+    audio.play();
+    document.getElementById('irra').style.backgroundColor = "#2FE630";
+    setTimeout(() => {
+        document.getElementById('irra').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function ratin(){
+    const audio = new Audio('ratin.mp3')
+    audio.play();
+    document.getElementById('ratin').style.backgroundColor = "#F87AFA";
+    setTimeout(() => {
+        document.getElementById('ratin').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function xarope(){
+    const audio = new Audio('xarope.mp3')
+    audio.play();
+    document.getElementById('xarope').style.backgroundColor = "#FADD3C"; 
+    setTimeout(() => {
+        document.getElementById('xarope').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
+function xi(){
+    const audio = new Audio('xi.mp3')
+    audio.play();
+    document.getElementById('xi').style.backgroundColor = "#C4FF5C";
+    setTimeout(() => {
+        document.getElementById('xi').style.backgroundColor = "rgb(221, 213, 213)";
+    }, 1000);
+}
+
